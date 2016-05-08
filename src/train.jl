5de1be68966a6e3af318d6ce04e3f3fbf871de5c
Aaron Gokaslan <Skylion.aaron@gmail.com>
2016-05-08 16:42:43 -0400

    Added parallelism to train.jl and improved efficiency of types.jl

--- src/train.jl
+++ src/train.jl
@@ -10,43 +10,42 @@
 # regularizer are the two standard gradient descent parameters that control
 # how fast the descent happens and prevent the descent from over-aggressive
 # training in any direction.
-
 function train(rating_set::RatingSet,
                max_rank;
                min_epochs=0,
                max_epochs=100,
                learning_rate=0.001,
                regularizer=0.02)
-    user_features = [0.1 for r=1:length(rating_set.user_to_index), c=1:max_rank]
-    item_features = [0.1 for r=1:length(rating_set.item_to_index), c=1:max_rank]
-    residuals = [Residual(rating.value, 0.0, 0.0) for rating in rating_set.training_set]
-    num_ratings = length(rating_set.training_set)
     p = Progress(max_rank, 1, "Computing truncated rank $(max_rank) SVD ")
+    training_set = convert(SharedArray, rating_set.training_set)
+    user_features = SharedArray(Float32, (length(rating_set.user_to_index), max_rank), init = S -> S[Base.localindexes(S)] = 0.1)
+    item_features = SharedArray(Float32, (length(rating_set.item_to_index), max_rank), init = S -> S[Base.localindexes(S)] = 0.1)
+    residuals = SharedArray(Residual, size(rating_set.training_set), init = [Residual(rating.value, 0.0, 0.0) for rating in rating_set.training_set])
+    num_ratings = length(rating_set.training_set)
     for rank=1:max_rank
-        errors = [0.0, Inf, Inf]
-        for i=1:max_epochs
-            for j=1:num_ratings
-                rating, residual = rating_set.training_set[j], residuals[j]
-                item_feature = item_features[rating.item, rank]
-                user_feature = user_features[rating.user, rank]
-                residual.curr_error = residual.value - user_feature * item_feature
-                error_diff = residual.prev_error - residual.curr_error
-                errors[1] += error_diff * error_diff
-                residual.prev_error = residual.curr_error
-                item_features[rating.item, rank] += learning_rate * (residual.curr_error * user_feature - regularizer * item_feature)
-                user_features[rating.user, rank] += learning_rate * (residual.curr_error * item_feature - regularizer * user_feature)
-            end
-            # distance decreases, then increases, then decreases. we want to catch it on second decrease
-            if i > min_epochs && errors[1] < errors[2] && errors[2] > errors[3]
-                break
-            end
-            errors[1], errors[2], errors[3] = 0.0, errors[1], errors[2]
+      errors = SharedArray(Float32, (3,), init = S -> [0.0, Inf, Inf])
+      for i=1:max_epochs
+        @sync @parallel for j=1:num_ratings
+          rating, residual = training_set[j], residuals[j]
+          item_feature = item_features[rating.item, rank]
+          user_feature = user_features[rating.user, rank]
+          error_diff = residual.prev_error - residual.curr_error
+          errors[1] += error_diff * error_diff
+          residuals[j] = Residual(residual.value, -user_feature * item_feature + residual.value, residual.curr_error)
+          item_features[rating.item, rank] += learning_rate * (residual.curr_error * user_feature - regularizer * item_feature)
+          user_features[rating.user, rank] += learning_rate * (residual.curr_error * item_feature - regularizer * user_feature)
         end
-        for residual in residuals
-            residual.value = residual.curr_error
-            residual.prev_error = 0.0
+        # distance decreases, then increases, then decreases. we want to catch it on second decrease
+        if i > min_epochs && errors[1] < errors[2] && errors[2] > errors[3]
+          break
         end
-        next!(p)
+        errors[1], errors[2], errors[3] = 0.0, errors[1], errors[2]
+      end
+      for j=1:num_ratings
+        residual = residuals[j]
+        residuals[j] = Residual(residual.curr_error, residual.curr_error, 0)
+      end
+      next!(p)
     end
 
     # We end up with just the U and V matrices of the singular value
